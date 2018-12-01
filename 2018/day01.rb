28860fe36ea12d78cd7af46044d0e61f0d89eaef
Kim Lethan <kim@skydning.dk>
2018-12-01 06:39:50 +0100

    Added 2018 day 1

---
+++ 2018/day01.rb
@@ -0,0 +1,17 @@
+file = File.open('input_day01.txt', 'r')
+changes = []
+while line = file.gets
+   changes << line.strip.to_i
+end
+file.close
+puts changes.sum
+
+seen = Hash.new(false)
+counter = 0
+frequency = 0
+while not seen[frequency]
+  seen[frequency] = true
+  frequency += changes[counter % changes.length]
+  counter += 1
+end
+puts frequency

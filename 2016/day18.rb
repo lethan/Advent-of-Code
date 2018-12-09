37eac32f81a846dd909a6ac01d9bd043629c0a05
Kim Lethan <kim@skydning.dk>
2018-12-09 16:06:46 +0100

    Added 2016 day 18

---
+++ 2016/day18.rb
@@ -0,0 +1,20 @@
+file = File.open('input_day18.txt', 'r')
+while (line = file.gets)
+  last_row = line.chomp.split('').map { |c| c == '.' }
+end
+file.close
+
+row_count = 1
+safe_tiles = last_row.select { |b| b }.length
+while row_count < 400_000
+  puts safe_tiles if row_count == 40
+  last_row = [true] + last_row + [true]
+  new_row = []
+  0.upto(last_row.length - 3) do |i|
+    new_row << ![[false, false, true], [true, false, false], [false, true, true], [true, true, false]].include?(last_row[i..(i + 2)])
+  end
+  last_row = new_row
+  safe_tiles += last_row.select { |b| b }.length
+  row_count += 1
+end
+puts safe_tiles

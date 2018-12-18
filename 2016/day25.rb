2d434d85315e5610a15ea8e38df4a52ddb10f822
Kim Lethan <kim@skydning.dk>
2018-12-18 23:35:31 +0100

    Added 2016 day 25

--- 2016/day25.rb
+++ 2016/day25.rb
@@ -1,22 +1,25 @@
+# frozen_string_literal: true
+
 file = File.open('input_day25.txt', 'r')
 program = []
-while line = file.gets
+while (line = file.gets)
   program << line.chomp
 end
 file.close
 
-def run(program, registers)
+def infinite_clock_signal(program, registers)
+  seen_states = Hash.new(false)
   pointer = 0
   last_out = nil
-  while pointer < program.length do
+  while pointer < program.length
     instruction = program[pointer].split(' ')
     case instruction[0]
     when 'cpy'
-      if instruction[1].to_i.to_s == instruction[1]
-        registers[instruction[2]] = instruction[1].to_i
-      else
-        registers[instruction[2]] = registers[instruction[1]]
-      end
+      registers[instruction[2]] = if instruction[1].to_i.to_s == instruction[1]
+                                    instruction[1].to_i
+                                  else
+                                    registers[instruction[1]]
+                                  end
       pointer += 1
     when 'inc'
       registers[instruction[1]] = registers[instruction[1]] + 1
@@ -26,28 +29,29 @@ def run(program, registers)
       pointer += 1
     when 'jnz'
       cmp = instruction[1].to_i.to_s == instruction[1] ? instruction[1].to_i : registers[instruction[1]]
-      if cmp == 0
-        pointer += 1
-      else
-        pointer += instruction[2].to_i
-      end
+      pointer += cmp.zero? ? 1 : instruction[2].to_i
     when 'out'
-      puts registers[instruction[1]]
       break unless [0, 1].include?(registers[instruction[1]])
       break if registers[instruction[1]] == last_out && !last_out.nil?
 
+      return true if seen_states[[registers.dup, pointer]]
+
+      seen_states[[registers.dup, pointer]] = true
       last_out = registers[instruction[1]]
       pointer += 1
     else
       puts "unknown instruction: #{program[pointer]}"
     end
   end
-  registers['a']
+  false
 end
 
 registers = Hash.new(0)
-registers['a'] = 0
-run(program.dup, registers)
-registers = Hash.new(0)
-registers['c'] = 1
-puts run(program, registers)
+counter = 0
+registers['a'] = counter
+until infinite_clock_signal(program.dup, registers)
+  counter += 1
+  registers = Hash.new(0)
+  registers['a'] = counter
+end
+puts counter

614229a4feef322af5087b8610779ed7f30f64a7
Kim Lethan <kim@skydning.dk>
2020-12-02 16:32:08 +0100

    Added 2020 day 2

---
+++ 2020/day02.exs
@@ -0,0 +1,44 @@
+defmodule Day2 do
+
+  def import(file) do
+    {:ok, content} = File.read(file)
+
+    content
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x -> split(x) end)
+  end
+
+  defp split(string) do
+    [min, max, letter, string] = Regex.run(~r/(?<min>[0-9]+)-(?<max>[0-9]+) (?<letter>.): (?<str>.+)/, string, capture: :all_but_first)
+    %{min: String.to_integer(min), max: String.to_integer(max), letter: letter, string: string}
+  end
+
+  def task1(input) do
+    input
+    |> Enum.filter(fn mp ->
+      count = mp.string |> String.graphemes |> Enum.count(& &1 == mp.letter)
+      count <= mp.max && count >= mp.min
+    end)
+    |> Enum.count
+  end
+
+  def task2(input) do
+    input
+    |> Enum.filter(fn mp ->
+      first = String.at(mp.string, mp.min - 1) == mp.letter
+      last = String.at(mp.string, mp.max - 1) == mp.letter
+      (first && !last || (!first && last))
+    end)
+    |> Enum.count
+  end
+end
+
+input = Day2.import("input_day02.txt")
+
+input
+|> Day2.task1
+|> IO.puts
+
+input
+|> Day2.task2
+|> IO.puts

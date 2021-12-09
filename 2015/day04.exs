5e7124d4e684483451bd5224f08ad682a82985d1
Kim Lethan <kim@skydning.dk>
2021-12-09 21:39:49 +0100

    Added 2015 day 4 elixir

---
+++ 2015/day04.exs
@@ -0,0 +1,38 @@
+defmodule AOC2015.Day4 do
+  defp find_valid_hash_value(input, number_of_zeroes, current_number \\ 1) do
+    zeroes = String.duplicate("0", number_of_zeroes)
+    test_value = input <> Integer.to_string(current_number)
+
+    result =
+      :crypto.hash(:md5, test_value)
+      |> Base.encode16()
+
+    cond do
+      String.starts_with?(result, zeroes) ->
+        current_number
+
+      true ->
+        find_valid_hash_value(input, number_of_zeroes, current_number + 1)
+    end
+  end
+
+  def task1(input) do
+    input
+    |> find_valid_hash_value(5)
+  end
+
+  def task2(input) do
+    input
+    |> find_valid_hash_value(6)
+  end
+end
+
+input = "bgvyzdsv"
+
+input
+|> AOC2015.Day4.task1()
+|> IO.puts()
+
+input
+|> AOC2015.Day4.task2()
+|> IO.puts()

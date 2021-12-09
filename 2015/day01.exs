30e319a01a3d5bcbcd328aa9745aaa777052f9ff
Kim Lethan <kim@skydning.dk>
2021-12-09 17:29:11 +0100

    Added 2015 day 1 elixir

---
+++ 2015/day01.exs
@@ -0,0 +1,51 @@
+defmodule AOC2015.Day1 do
+  def import(file) do
+    {:ok, content} = File.read(file)
+
+    content
+    |> String.trim()
+    |> String.graphemes()
+  end
+
+  defp end_floor(list), do: end_floor(list, 0)
+  defp end_floor([], current_floor), do: current_floor
+
+  defp end_floor(["(" | rest], current_floor) do
+    end_floor(rest, current_floor + 1)
+  end
+
+  defp end_floor([")" | rest], current_floor) do
+    end_floor(rest, current_floor - 1)
+  end
+
+  defp find_floor(list, find), do: find_floor(list, find, 0, 0)
+  defp find_floor(_list, find, current_floor, step) when find == current_floor, do: step
+
+  defp find_floor(["(" | rest], find, current_floor, step) do
+    find_floor(rest, find, current_floor + 1, step + 1)
+  end
+
+  defp find_floor([")" | rest], find, current_floor, step) do
+    find_floor(rest, find, current_floor - 1, step + 1)
+  end
+
+  defp find_floor([], _find, _current_floor, _step), do: raise("Unable to find floor")
+
+  def task1(input) do
+    end_floor(input)
+  end
+
+  def task2(input) do
+    find_floor(input, -1)
+  end
+end
+
+input = AOC2015.Day1.import("input_day01.txt")
+
+input
+|> AOC2015.Day1.task1()
+|> IO.puts()
+
+input
+|> AOC2015.Day1.task2()
+|> IO.puts()

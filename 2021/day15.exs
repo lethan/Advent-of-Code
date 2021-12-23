f9e0dc8ea48ace004ca6449771ac1926894105e0
Kim Lethan <kim@skydning.dk>
2021-12-23 18:35:45 +0100

    refactoring 2021 day 15

--- 2021/day15.exs
+++ 2021/day15.exs
@@ -1,3 +1,5 @@
+Code.require_file("priority_queue.ex")
+
 defmodule CaveMap do
   @derive {Inspect, only: [:original_size, :dimension]}
   defstruct original_size: nil, map: nil, dimension: nil
@@ -49,45 +51,6 @@ defmodule CaveMap do
 
         rem(value + steps_x + steps_y - 1, 9) + 1
     end
-  end
-end
-
-defmodule PriorityQueue do
-  defstruct queue: nil
-
-  def new() do
-    %__MODULE__{queue: :gb_trees.empty()}
-  end
-
-  def insert(%__MODULE__{queue: queue}, priority, element) do
-    new_value =
-      case :gb_trees.lookup(priority, queue) do
-        :none ->
-          [element]
-
-        {:value, list} ->
-          [element | list]
-      end
-
-    new_queue = :gb_trees.enter(priority, new_value, queue)
-
-    %__MODULE__{queue: new_queue}
-  end
-
-  def get(%__MODULE__{queue: queue}) do
-    {priority, list, new_queue} = :gb_trees.take_smallest(queue)
-
-    {element, new_queue} =
-      case list do
-        [element] ->
-          {element, new_queue}
-
-        [element | rest] ->
-          new_queue = :gb_trees.enter(priority, rest, new_queue)
-          {element, new_queue}
-      end
-
-    {priority, element, %__MODULE__{queue: new_queue}}
   end
 end
 

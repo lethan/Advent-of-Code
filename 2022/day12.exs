9b3151c48d895054b69d3c1d1cd8155078f08368
Kim Lethan <kim@skydning.dk>
2022-12-12 12:51:45 +0100

    Improved 2022 day 12

--- 2022/day12.exs
+++ 2022/day12.exs
@@ -50,46 +50,87 @@ defmodule AOC2022.Day12 do
   end
 
   def shortest_path(queue = %PriorityQueue{}, visited, map, destination) do
-    case PriorityQueue.empty?(queue) do
+    {steps, coord = {x, y}, new_queue} = PriorityQueue.get(queue)
+    current_height = Map.get(map, coord)
+
+    case coord == destination do
       true ->
-        nil
+        steps
 
-      _ ->
-        {steps, coord = {x, y}, new_queue} = PriorityQueue.get(queue)
-        current_height = Map.get(map, coord)
+      false ->
+        {new_queue, visited} =
+          for(
+            diff_x <- -1..1,
+            diff_y <- -1..1,
+            abs(diff_x) != abs(diff_y),
+            do: {x + diff_x, y + diff_y}
+          )
+          |> Enum.reduce({new_queue, visited}, fn visit_coord,
+                                                  acc = {current_queue, current_visisted} ->
+            case MapSet.member?(visited, visit_coord) do
+              false ->
+                case Map.get(map, visit_coord) do
+                  height when not is_nil(height) and height <= current_height + 1 ->
+                    {PriorityQueue.insert(current_queue, steps + 1, visit_coord),
+                     MapSet.put(current_visisted, visit_coord)}
 
-        case coord == destination do
-          true ->
-            steps
-
-          false ->
-            {new_queue, visited} =
-              for(
-                diff_x <- -1..1,
-                diff_y <- -1..1,
-                abs(diff_x) != abs(diff_y),
-                do: {x + diff_x, y + diff_y}
-              )
-              |> Enum.reduce({new_queue, visited}, fn visit_coord,
-                                                      acc = {current_queue, current_visisted} ->
-                case MapSet.member?(visited, visit_coord) do
-                  false ->
-                    case Map.get(map, visit_coord) do
-                      height when not is_nil(height) and height <= current_height + 1 ->
-                        {PriorityQueue.insert(current_queue, steps + 1, visit_coord),
-                         MapSet.put(current_visisted, visit_coord)}
-
-                      _ ->
-                        acc
-                    end
-
-                  true ->
+                  _ ->
                     acc
                 end
-              end)
 
-            shortest_path(new_queue, visited, map, destination)
-        end
+              true ->
+                acc
+            end
+          end)
+
+        shortest_path(new_queue, visited, map, destination)
+    end
+  end
+
+  def find_shortest_start_path(map, start, find_elevation \\ 0) do
+    find_shortest_start_path(
+      PriorityQueue.new() |> PriorityQueue.insert(0, start),
+      MapSet.new([start]),
+      map,
+      find_elevation
+    )
+  end
+
+  def find_shortest_start_path(queue = %PriorityQueue{}, visited, map, find_elevation) do
+    {steps, coord = {x, y}, new_queue} = PriorityQueue.get(queue)
+    current_height = Map.get(map, coord)
+
+    case current_height == find_elevation do
+      true ->
+        steps
+
+      false ->
+        {new_queue, visited} =
+          for(
+            diff_x <- -1..1,
+            diff_y <- -1..1,
+            abs(diff_x) != abs(diff_y),
+            do: {x + diff_x, y + diff_y}
+          )
+          |> Enum.reduce({new_queue, visited}, fn visit_coord,
+                                                  acc = {current_queue, current_visisted} ->
+            case MapSet.member?(visited, visit_coord) do
+              false ->
+                case Map.get(map, visit_coord) do
+                  height when not is_nil(height) and height >= current_height - 1 ->
+                    {PriorityQueue.insert(current_queue, steps + 1, visit_coord),
+                     MapSet.put(current_visisted, visit_coord)}
+
+                  _ ->
+                    acc
+                end
+
+              true ->
+                acc
+            end
+          end)
+
+        find_shortest_start_path(new_queue, visited, map, find_elevation)
     end
   end
 
@@ -97,15 +138,8 @@ defmodule AOC2022.Day12 do
     shortest_path(map, start, destination)
   end
 
-  def task2({map, start, destination}) do
-    map
-    |> Enum.filter(fn {_, height} -> height == 0 end)
-    |> Enum.map(fn {coord, _} -> coord end)
-    |> Enum.map(fn start ->
-      shortest_path(map, start, destination)
-    end)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.min()
+  def task2({map, _start, destination}) do
+    find_shortest_start_path(map, destination)
   end
 end
 

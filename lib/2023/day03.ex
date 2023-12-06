64eb642e728b5900c7985e3f68d6f2ff81ed5451
Kim Lethan <kim@skydning.dk>
2023-12-06 14:21:18 +0100

    Changed formatting

--- lib/2023/day03.ex
+++ lib/2023/day03.ex
@@ -2,38 +2,49 @@ defmodule AOC.Year2023.Day3 do
   def import(file) do
     content = File.read!(file)
 
-    {map, values, symbols, _, _} = content
-    |> String.split("\n", trim: true)
-    |> Enum.reduce({%{}, %{}, %{}, 0, 0}, fn str, {map, values, symbols, current_number, row} ->
-      {map, values, symbols, current_number, _, is_number} =
-        String.graphemes(str)
-        |> Enum.reduce({map, values, symbols, current_number, 0, false}, fn graph,
-                                                                            {map, values, symbols,
-                                                                             current_number,
-                                                                             column, is_number} ->
-          case graph do
-            "." ->
-              current_number = if is_number, do: current_number + 1, else: current_number
-              {map, values, symbols, current_number, column + 1, false}
+    {map, values, symbols, _, _} =
+      content
+      |> String.split("\n", trim: true)
+      |> Enum.reduce({%{}, %{}, %{}, 0, 0}, fn str, {map, values, symbols, current_number, row} ->
+        {map, values, symbols, current_number, _, is_number} =
+          String.graphemes(str)
+          |> Enum.reduce({map, values, symbols, current_number, 0, false}, fn graph,
+                                                                              {map, values,
+                                                                               symbols,
+                                                                               current_number,
+                                                                               column,
+                                                                               is_number} ->
+            case graph do
+              "." ->
+                current_number = if is_number, do: current_number + 1, else: current_number
+                {map, values, symbols, current_number, column + 1, false}
 
-            x when x in ~w{0 1 2 3 4 5 6 7 8 9} ->
-              number = String.to_integer(x)
-              values = Map.update(values, current_number, number, fn val -> val * 10 + number end)
-              map = Map.put(map, {column, row}, current_number)
-              {map, values, symbols, current_number, column + 1, true}
+              x when x in ~w{0 1 2 3 4 5 6 7 8 9} ->
+                number = String.to_integer(x)
 
-            symbol ->
-              current_number = if is_number, do: current_number + 1, else: current_number
-              map = Map.put(map, {column, row}, symbol)
-              symbols = Map.update(symbols, symbol, [{column, row}], fn list -> [{column, row} | list] end)
-              {map, values, symbols, current_number, column + 1, false}
-          end
-        end)
+                values =
+                  Map.update(values, current_number, number, fn val -> val * 10 + number end)
 
-      current_number = if is_number, do: current_number + 1, else: current_number
+                map = Map.put(map, {column, row}, current_number)
+                {map, values, symbols, current_number, column + 1, true}
 
-      {map, values, symbols, current_number, row + 1}
-    end)
+              symbol ->
+                current_number = if is_number, do: current_number + 1, else: current_number
+                map = Map.put(map, {column, row}, symbol)
+
+                symbols =
+                  Map.update(symbols, symbol, [{column, row}], fn list ->
+                    [{column, row} | list]
+                  end)
+
+                {map, values, symbols, current_number, column + 1, false}
+            end
+          end)
+
+        current_number = if is_number, do: current_number + 1, else: current_number
+
+        {map, values, symbols, current_number, row + 1}
+      end)
 
     {map, values, symbols}
   end
@@ -44,7 +55,12 @@ defmodule AOC.Year2023.Day3 do
     symbols
     |> Enum.reduce(MapSet.new(), fn {_symbol, list}, acc ->
       Enum.reduce(list, acc, fn {x, y}, acc ->
-        (for new_x <- x-1..x+1, new_y <- y-1..y+1, new_x != x or new_y != y, do: {new_x, new_y})
+        for(
+          new_x <- (x - 1)..(x + 1),
+          new_y <- (y - 1)..(y + 1),
+          new_x != x or new_y != y,
+          do: {new_x, new_y}
+        )
         |> Enum.reduce(acc, fn coord, acc ->
           val = Map.get(map, coord)
           if is_integer(val), do: MapSet.put(acc, val), else: acc
@@ -59,15 +75,23 @@ defmodule AOC.Year2023.Day3 do
 
     Map.get(symbols, "*")
     |> Enum.reduce(0, fn {x, y}, acc ->
-      mapset = (for new_x <- x-1..x+1, new_y <- y-1..y+1, new_x != x or new_y != y, do: {new_x, new_y})
-      |> Enum.reduce(MapSet.new(), fn coord, mapset ->
-        val = Map.get(map, coord)
-        if is_integer(val), do: MapSet.put(mapset, val), else: mapset
-      end)
+      mapset =
+        for(
+          new_x <- (x - 1)..(x + 1),
+          new_y <- (y - 1)..(y + 1),
+          new_x != x or new_y != y,
+          do: {new_x, new_y}
+        )
+        |> Enum.reduce(MapSet.new(), fn coord, mapset ->
+          val = Map.get(map, coord)
+          if is_integer(val), do: MapSet.put(mapset, val), else: mapset
+        end)
+
       if MapSet.size(mapset) == 2 do
-        gear = MapSet.to_list(mapset)
-        |> Enum.map(&Map.get(values, &1))
-        |> Enum.product()
+        gear =
+          MapSet.to_list(mapset)
+          |> Enum.map(&Map.get(values, &1))
+          |> Enum.product()
 
         gear + acc
       else
--- lib/2023/day04.ex
+++ lib/2023/day04.ex
@@ -5,21 +5,24 @@ defmodule AOC.Year2023.Day4 do
     content
     |> String.split("\n", trim: true)
     |> Enum.map(fn str ->
-      [card, numbers] = str
-      |> String.split(": ")
+      [card, numbers] =
+        str
+        |> String.split(": ")
 
       [_, cardnumber] = String.split(card, " ", trim: true)
       cardnumber = String.to_integer(cardnumber)
 
       [winning_numbers, numbers_on_card] = String.split(numbers, " | ")
 
-      winning_numbers = winning_numbers
-      |> String.split(" ", trim: true)
-      |> Enum.map(&String.to_integer/1)
+      winning_numbers =
+        winning_numbers
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
 
-      numbers_on_card = numbers_on_card
-      |> String.split(" ", trim: true)
-      |> Enum.map(&String.to_integer/1)
+      numbers_on_card =
+        numbers_on_card
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
 
       {cardnumber, winning_numbers, numbers_on_card}
     end)
@@ -28,13 +31,14 @@ defmodule AOC.Year2023.Day4 do
   def task1(input) do
     input
     |> Enum.reduce(0, fn {_, winning_numbers, numbers}, acc ->
-      acc + Enum.reduce(winning_numbers, 0, fn winner, acc ->
-        if winner in numbers do
-          if acc == 0, do: 1, else: acc * 2
-        else
-          acc
-        end
-      end)
+      acc +
+        Enum.reduce(winning_numbers, 0, fn winner, acc ->
+          if winner in numbers do
+            if acc == 0, do: 1, else: acc * 2
+          else
+            acc
+          end
+        end)
     end)
   end
 
@@ -44,15 +48,16 @@ defmodule AOC.Year2023.Day4 do
       extra = Map.update(extra, card, 1, fn x -> x + 1 end)
       copies = Map.get(extra, card)
 
-      {extra,_ } = Enum.reduce(winning_numbers, {extra, 0}, fn winner, {extra, count} = acc ->
-        if winner in numbers do
-          count = count + 1
-          extra = Map.update(extra, card + count, copies, fn x -> x + copies end)
-          {extra, count}
-        else
-          acc
-        end
-      end)
+      {extra, _} =
+        Enum.reduce(winning_numbers, {extra, 0}, fn winner, {extra, count} = acc ->
+          if winner in numbers do
+            count = count + 1
+            extra = Map.update(extra, card + count, copies, fn x -> x + copies end)
+            {extra, count}
+          else
+            acc
+          end
+        end)
 
       {extra, total_cards + copies}
     end)

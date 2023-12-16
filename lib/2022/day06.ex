4a9d6a2f855b9d7f0489e889e0e2bf419d98a28d
Kim Lethan <kim@skydning.dk>
2023-12-16 21:40:33 +0100

    Moved 2022 day 6 to mix

---
+++ lib/2022/day06.ex
@@ -0,0 +1,57 @@
+defmodule AoC.Year2022.Day6 do
+  alias AoC.Zipper
+
+  def import(file) do
+    {:ok, content} = File.read(file)
+
+    content
+    |> String.trim()
+  end
+
+  def find_unique_chars(
+        <<char::binary-1, rest::binary>>,
+        number_of_chars \\ 4,
+        counter \\ 1,
+        zipper \\ Zipper.new()
+      ) do
+    zipper = Zipper.enqueue(zipper, char)
+
+    case Zipper.size(zipper) do
+      ^number_of_chars ->
+        zipper
+        |> Enum.uniq()
+        |> Enum.count()
+        |> case do
+          ^number_of_chars ->
+            counter
+
+          _ ->
+            {_, zipper} = Zipper.dequeue(zipper)
+            find_unique_chars(rest, number_of_chars, counter + 1, zipper)
+        end
+
+      _ ->
+        find_unique_chars(rest, number_of_chars, counter + 1, zipper)
+    end
+  end
+
+  def task1(input) do
+    input
+    |> find_unique_chars
+  end
+
+  def task2(input) do
+    input
+    |> find_unique_chars(14)
+  end
+end
+
+input = AoC.Year2022.Day6.import("input/2022/input_day06.txt")
+
+input
+|> AoC.Year2022.Day6.task1()
+|> IO.puts()
+
+input
+|> AoC.Year2022.Day6.task2()
+|> IO.puts()

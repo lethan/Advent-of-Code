cfec3d3e672983b9d58106a62e5116bb95baf80e
Kim Lethan <kim@skydning.dk>
2024-12-25 17:07:12 +0100

    Moved 2020 day 5

--- lib/2020/day05.ex
+++ lib/2020/day05.ex
@@ -1,5 +1,4 @@
-defmodule Day5 do
-
+defmodule AoC.Year2020.Day5 do
   def import(file) do
     {:ok, content} = File.read(file)
 
@@ -7,12 +6,12 @@ defmodule Day5 do
     |> String.split("\n", trim: true)
   end
 
-  def seat_id(boardingpass) do
+  defp seat_id(boardingpass) do
     boardingpass
-    |> String.graphemes
+    |> String.graphemes()
     |> Enum.map(&binary_value/1)
-    |> Enum.reverse
-    |> Enum.reduce({0, 1}, fn x, {value, base} -> {value + x * base, base*2} end)
+    |> Enum.reverse()
+    |> Enum.reduce({0, 1}, fn x, {value, base} -> {value + x * base, base * 2} end)
     |> elem(0)
   end
 
@@ -20,16 +19,19 @@ defmodule Day5 do
     case letter do
       "B" ->
         1
+
       "F" ->
         0
+
       "R" ->
         1
+
       "L" ->
         0
     end
   end
 
-  def find_vacant([current | passes], last_value \\ nil) do
+  defp find_vacant([current | passes], last_value \\ nil) do
     if last_value do
       if last_value + 1 == current do
         find_vacant(passes, current)
@@ -44,23 +46,23 @@ defmodule Day5 do
   def task1(input) do
     input
     |> Enum.map(&seat_id/1)
-    |> Enum.max
+    |> Enum.max()
   end
 
   def task2(input) do
     input
     |> Enum.map(&seat_id/1)
-    |> Enum.sort
-    |> find_vacant
+    |> Enum.sort()
+    |> find_vacant()
   end
 end
 
-input = Day5.import("input_day05.txt")
+# input = AoC.Year2020.Day5.import("input/2020/input_day05.txt")
 
-input
-|> Day5.task1
-|> IO.puts
+# input
+# |> AoC.Year2020.Day5.task1()
+# |> IO.puts()
 
-input
-|> Day5.task2
-|> IO.puts
+# input
+# |> AoC.Year2020.Day5.task2()
+# |> IO.puts()

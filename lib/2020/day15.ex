0ff851913984cc490adffac61ae2830888d645c7
Kim Lethan <kim@skydning.dk>
2024-12-27 21:34:17 +0100

    Moved 2020 day 15 to lib

--- lib/2020/day15.ex
+++ lib/2020/day15.ex
@@ -1,16 +1,28 @@
-defmodule Day15 do
+defmodule AoC.Year2020.Day15 do
+  def import(file) do
+    {:ok, content} = File.read(file)
+
+    content
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
 
   defp play_game(game, stop) do
     last_value = Map.get(game, :last_value)
     seen_last_value = Map.get(game, last_value)
-    {new_value, index} = case seen_last_value do
-      {index, nil} ->
-        {0, index}
-      {index, index2} ->
-        {index - index2, index}
-    end
+
+    {new_value, index} =
+      case seen_last_value do
+        {index, nil} ->
+          {0, index}
+
+        {index, index2} ->
+          {index - index2, index}
+      end
+
     game = Map.put(game, :last_value, new_value)
     game = Map.update(game, new_value, {index + 1, nil}, fn {existing_value, _} -> {index + 1, existing_value} end)
+
     if index + 1 == stop do
       game
     else
@@ -22,7 +34,7 @@ defmodule Day15 do
     game = %{}
 
     input
-    |> Stream.with_index
+    |> Stream.with_index()
     |> Enum.reduce(game, fn {value, index}, acc ->
       game = Map.put(acc, :last_value, value)
       Map.update(game, value, {index + 1, nil}, fn {existing_value, _} -> {index + 1, existing_value} end)
@@ -39,17 +51,17 @@ defmodule Day15 do
   def task2(input) do
     input
     |> initial_game
-    |> play_game(30000000)
+    |> play_game(30_000_000)
     |> Map.get(:last_value)
   end
 end
 
-input = [1, 17, 0, 10, 18, 11, 6]
+# input = AoC.Year2020.Day15.import("input/2020/input_day15.txt")
 
-input
-|> Day15.task1
-|> IO.puts
+# input
+# |> AoC.Year2020.Day15.task1()
+# |> IO.puts()
 
-input
-|> Day15.task2
-|> IO.puts
+# input
+# |> AoC.Year2020.Day15.task2()
+# |> IO.puts()

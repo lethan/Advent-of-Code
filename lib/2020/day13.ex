ddb5a68404d918a0ef3559fdeadc280243366098
Kim Lethan <kim@skydning.dk>
2024-12-27 20:39:29 +0100

    Moved 2020 day 13 to lib

--- lib/2020/day13.ex
+++ lib/2020/day13.ex
@@ -1,30 +1,39 @@
-defmodule Day13 do
-
+defmodule AoC.Year2020.Day13 do
   def import(file) do
     {:ok, content} = File.read(file)
 
     content
     |> String.split("\n", trim: true)
-    |> convert_to_data
+    |> convert_to_data()
   end
 
   defp convert_to_data([start_time, bus_list]) do
-    {String.to_integer(start_time), Enum.map(String.split(bus_list, ","), &(if &1 == "x" do &1 else String.to_integer(&1) end))}
+    {String.to_integer(start_time),
+     Enum.map(
+       String.split(bus_list, ","),
+       &if &1 == "x" do
+         &1
+       else
+         String.to_integer(&1)
+       end
+     )}
   end
 
   defp find_first_bus(time, bus_list) do
     case Enum.find(bus_list, &(rem(time, &1) == 0)) do
       nil ->
-        find_first_bus(time+1, bus_list)
+        find_first_bus(time + 1, bus_list)
+
       bus ->
         {time, bus}
     end
   end
 
   defp find_multiplier([], value, _), do: value
+
   defp find_multiplier(list = [{bus, shift} | rest], value, multi) do
     if rem(value + shift, bus) == 0 do
-      find_multiplier(rest, value, bus*multi)
+      find_multiplier(rest, value, bus * multi)
     else
       find_multiplier(list, value + multi, multi)
     end
@@ -38,18 +47,18 @@ defmodule Day13 do
 
   def task2({_, bus_list}) do
     bus_list
-    |> Enum.with_index
+    |> Enum.with_index()
     |> Enum.reject(fn {bus, _} -> bus == "x" end)
-    |> find_multiplier(1,1)
+    |> find_multiplier(1, 1)
   end
 end
 
-input = Day13.import("input_day13.txt")
+# input = AoC.Year2020.Day13.import("input/2020/input_day13.txt")
 
-input
-|> Day13.task1
-|> IO.puts
+# input
+# |> AoC.Year2020.Day13.task1()
+# |> IO.puts()
 
-input
-|> Day13.task2
-|> IO.puts
+# input
+# |> AoC.Year2020.Day13.task2()
+# |> IO.puts()

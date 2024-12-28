9f561ea800797e6d72c9e0e17e4f4bff04dd8b8a
Kim Lethan <kim@skydning.dk>
2024-12-28 17:59:53 +0100

    Moved 2020 day 24 to lib

--- lib/2020/day24.ex
+++ lib/2020/day24.ex
@@ -1,5 +1,4 @@
-defmodule Day24 do
-
+defmodule AoC.Year2020.DDay24 do
   @directions ~w(east west northeast northwest southeast southwest)a
 
   def import(file) do
@@ -9,19 +8,24 @@ defmodule Day24 do
     |> String.split("\n", trim: true)
     |> Enum.map(fn x ->
       Regex.scan(~r/(se|sw|ne|nw|e|w)/, x, capture: :all_but_first)
-      |> List.flatten
+      |> List.flatten()
       |> Enum.map(fn y ->
         case y do
           "e" ->
             :east
+
           "w" ->
             :west
+
           "se" ->
             :southeast
+
           "sw" ->
             :southwest
+
           "ne" ->
             :northeast
+
           "nw" ->
             :northwest
         end
@@ -29,35 +33,41 @@ defmodule Day24 do
     end)
   end
 
-  def next_coordinate(direction, {x, y}) do
+  defp next_coordinate(direction, {x, y}) do
     case direction do
       :east ->
         {x + 2, y}
+
       :west ->
         {x - 2, y}
+
       :northeast ->
         {x + 1, y + 1}
+
       :southwest ->
         {x - 1, y - 1}
+
       :northwest ->
         {x - 1, y + 1}
+
       :southeast ->
         {x + 1, y - 1}
     end
   end
 
-  def flip_tile(coord, map) do
+  defp flip_tile(coord, map) do
     Map.update(map, coord, :black, fn existing_value ->
       case existing_value do
         :white ->
           :black
+
         :black ->
           :white
       end
     end)
   end
 
-  def tile_map(directions) do
+  defp tile_map(directions) do
     directions
     |> Enum.reduce(%{}, fn directions, acc ->
       Enum.reduce(directions, {0, 0}, &next_coordinate/2)
@@ -65,7 +75,7 @@ defmodule Day24 do
     end)
   end
 
-  def number_black_neighbors(map) do
+  defp number_black_neighbors(map) do
     map
     |> Enum.filter(fn {_, val} ->
       val == :black
@@ -77,14 +87,15 @@ defmodule Day24 do
     end)
   end
 
-  def new_day_map(map) do
+  defp new_day_map(map) do
     neighbors = number_black_neighbors(map)
 
-    new_map = neighbors
-    |> Enum.filter(&(elem(&1, 1) == 2))
-    |> Enum.reduce(%{}, fn {coord, _}, acc ->
-      Map.put(acc, coord, :black)
-    end)
+    new_map =
+      neighbors
+      |> Enum.filter(&(elem(&1, 1) == 2))
+      |> Enum.reduce(%{}, fn {coord, _}, acc ->
+        Map.put(acc, coord, :black)
+      end)
 
     neighbors
     |> Enum.filter(&(elem(&1, 1) == 1))
@@ -104,20 +115,21 @@ defmodule Day24 do
   end
 
   def task2(input) do
-    map = input
-    |> tile_map
+    map =
+      input
+      |> tile_map
 
     Enum.reduce(1..100, map, fn _, acc -> new_day_map(acc) end)
-    |> Enum.count
+    |> Enum.count()
   end
 end
 
-input = Day24.import("input_day24.txt")
+# input = AoC.Year2020.DDay24.import("input/2020/input_day24.txt")
 
-input
-|> Day24.task1
-|> IO.puts
+# input
+# |> AoC.Year2020.DDay24.task1()
+# |> IO.puts()
 
-input
-|> Day24.task2
-|> IO.puts
+# input
+# |> AoC.Year2020.DDay24.task2()
+# |> IO.puts()
